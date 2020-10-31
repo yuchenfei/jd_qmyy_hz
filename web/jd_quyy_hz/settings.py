d92e13378b6cb6fb0ec3235a6579065e9337ce5c
yuchenfei <954833373@qq.com>
2020-11-01 04:27:01 +0800

    configure postgres

--- web/jd_quyy_hz/settings.py
+++ web/jd_quyy_hz/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'jd_quyy_hz.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get("SQL_ENGINE", "django.db.backends.sqlite3"),
+        'NAME': os.environ.get("SQL_DATABASE", BASE_DIR / 'db.sqlite3'),
+        "USER": os.environ.get("SQL_USER", "user"),
+        "PASSWORD": os.environ.get("SQL_PASSWORD", "password"),
+        "HOST": os.environ.get("SQL_HOST", "localhost"),
+        "PORT": os.environ.get("SQL_PORT", "5432"),
     }
 }
 
