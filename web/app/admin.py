c71bdc2a714320dd138bd76328b43714e5f0c968
yuchenfei <954833373@qq.com>
2020-11-03 14:49:03 +0800

    feat: 开启后台管理

--- web/app/admin.py
+++ web/app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserExtension, Log
+
+
+class CustomModelAdmin(admin.ModelAdmin):
+    def __init__(self, model, admin_site):
+        self.list_display = [
+            field.name for field in model._meta.fields if field.name != "id"
+        ]
+        super(CustomModelAdmin, self).__init__(model, admin_site)
+
+
+admin.site.register(UserExtension, CustomModelAdmin)
+admin.site.register(Log, CustomModelAdmin)
